84f165a2ddfbb38ca787298bb2b4010286b65825
Colton <colton.foremaster@gmail.com>
2018-03-01 00:29:50 -0700

    First Commit, created new files

---
+++ ClickAuto.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+el:'#JobData',
+data:{
+  toGetTitle:'First Name',
+  toGetInfo:'',
+  toGetTemp:'Enter your first name here',
+  custFirst:'',
+  custLast:'',
+  custEmail:'',
+  carMake:'',
+  carModel:'',
+  problem:'',
+  custComments:'',
+
+},
+methods:{
+
+}
+});
